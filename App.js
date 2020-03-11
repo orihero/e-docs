3f1dfdc7a9e2dcc329c8663ea09571dd74ece258
KAMURANBEK <kamuranbek1998@gmail.com>
2020-03-11 19:06:22 +0500

    uploading project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+let App = () => {
+  return (
+    <View>
+      <Text>some text</Text>
+    </View>
+  );
+};
+
+export default App;
